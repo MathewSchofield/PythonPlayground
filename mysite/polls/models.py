6035dc3c6ef5246ee1f57e844fb56240f535f4ea
MathewSchofield <mathewschofield@hotmail.com>
2023-11-22 17:18:13 +0000

    added static files and tests

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,20 +1,26 @@
+""" The models for the poll app. """
+
 import datetime
 from django.db import models
 from django.utils import timezone
 
 class Question(models.Model):
+    """ The Question model. """ 
     def __str__(self):
-        return self.question_text
-    
-    def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        return str(self.question_text)
 
+    def was_published_recently(self):
+        """ Was the Question published within the last day? """
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+    
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField("date published")
 
 class Choice(models.Model):
+    """ The Choice model. """
     def __str__(self):
-        return self.choice_text
+        return str(self.choice_text)
 
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,30 +1,42 @@
-from django.http import HttpRequest, HttpResponseRedirect
+""" Views for the poll app. """
+from typing import Any
+from django.db import models
+from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
 from django.views import generic
+from django.utils import timezone
 from .models import Question, Choice
 
 
 class IndexView(generic.ListView):
+    """ The Index generic List view. """
     template_name = "polls/index.html"
     context_object_name = "latest_question_list"
 
     def get_queryset(self):
-        """ Return the last five published questions. """
-        return Question.objects.order_by("-pub_date")[:5]
+        """ Return the last five published questions (not including those set to be published in the future). """
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by("-pub_date")[:5]
 
 
 class DetailView(generic.DetailView):
+    """ The Detail generic detail view. """
     model = Question
     template_name = "polls/detail.html"
 
+    def get_queryset(self):
+        """ Excludes any questions that aren't published yet. """
+        return Question.objects.filter(pub_date__lte=timezone.now())
+
 
 class ResultsView(generic.DetailView):
+    """ The Results generic detail view. """
     model = Question
     template_name = "polls/results.html"
 
 
 def vote(request, question_id):
+    """ The Vote view. """
     question = get_object_or_404(Question, pk=question_id)
     try:
         selected_choice = question.choice_set.get(pk=request.POST["choice"])
